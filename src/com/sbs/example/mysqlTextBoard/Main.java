cd20098f2057c2bd5f652829ccb6e405b96d846e
lucia6758 <lucia6758@gmail.com>
2020-12-30 17:59:15 +0900

    http request 후 응답 받아오는 함수구현

--- src/com/sbs/example/mysqlTextBoard/Main.java
+++ src/com/sbs/example/mysqlTextBoard/Main.java
@@ -1,7 +1,19 @@
 package com.sbs.example.mysqlTextBoard;
 
+import com.sbs.example.mysqlTextBoard.Util.Util;
+
 public class Main {
 	public static void main(String[] args) {
-		new App().run();
+		//new App().run();
+		
+		testApi();
+	}
+
+	private static void testApi() {
+		String url = "https://disqus.com/api/3.0/forums/listThreads.json";
+		
+		String rs = Util.callApi(url, "api_key=BaIQq3yDal19LyyTlIPtRYUi3LpDivsZAwaJC5HUCtZ8VTuvL63eFEec7yx8k9y8","forum=klvs-blog", "thread:ident=article_5.html");
+		System.out.println(rs);
+		
 	}
 }
