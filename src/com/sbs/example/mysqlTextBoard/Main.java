65e7777818fc8e91e1ec8b04355b1c70f959e918
lucia6758 <lucia6758@gmail.com>
2021-01-03 03:57:05 +0900

    환경변수 셋팅 및 테스트 & 메이븐프로젝트로 변경 후 google analystics api 라이브러리 추가

--- src/com/sbs/example/mysqlTextBoard/Main.java
+++ src/com/sbs/example/mysqlTextBoard/Main.java
@@ -4,8 +4,8 @@ import com.sbs.example.mysqlTextBoard.test.TestRunner;
 
 public class Main {
 	public static void main(String[] args) {
-		new App().run();
+		// new App().run();
 
-		// new TestRunner().run();
+		new TestRunner().run();
 	}
 }
